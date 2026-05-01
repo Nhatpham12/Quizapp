a40ac899f052b8b67d98d78f0e89c5206020d2ea
NhatPham12 <nhat88672@gmail.com>
2026-05-01 14:23:37 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Quiz from "./components/Quiz";
+import Result from "./components/Result";
+
+function App() {
+  return (
+    <div className="container">
+      <h1> quiz vui lập trình</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Result.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Result = ({ score, totalQuestionNum, restartQuiz, rewatchQuiz }) => {
+  return (
+    <div>
+      <h2>Kết quả</h2>
+      <p className="result">
+        Bạn đã trả lời đúng {score}/{totalQuestionNum} câu
+      </p>
+      <div className="resultButtonsContainer">
+        <button className="result-button" onClick={rewatchQuiz}>
+          Xem lại
+        </button>
+        <button className="result-button" onClick={restartQuiz}>
+          Làm lại
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Result;
